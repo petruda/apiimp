2f8bfc517547571fa851dcc138745bbac287fbd0
petruda <petru650@gmail.com>
2023-10-19 15:53:04 +0300

    Finished the styling, added a third API integration for ISBN search

--- server.js
+++ server.js
@@ -21,32 +21,63 @@ app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
     res.render("index.ejs", {    
-      anime: "Click the button first",
-      character: "Click the button first",
-      quote: "Click the button first",
-      urlImage : "Click the button first", 
+      anime: "",
+      character: "",
+      quote: "",
+      urlImage : "https://nekos.best/api/v2/hug/5721b0f4-a2ed-4de6-9ea3-763601c72fc9.gif", 
+      isbnData : "https://covers.openlibrary.org/b/id/8538817-S.jpg",
     });
   });
 app.post("/", async (req, res) => {
   try {
     const result = await axios.get("https://animechan.xyz/api/random");
     const resultImage = await axios.get("https://nekos.best/api/v2/hug?amount=1");
-    var resultImgStr = JSON.stringify(resultImage.data.results);
+    var resultImg = resultImage.data.results[0].url;
     
 
     res.render("index.ejs", { 
       anime: result.data.anime,
       character: result.data.character,
       quote: result.data.quote,
-      urlImage : resultImageStr.data.url,
+      urlImage : resultImg,
+      isbnData : "https://covers.openlibrary.org/b/id/8538817-S.jpg",
     });
     console.log(result);
-    console.log(resultImageStr.data.url);
+    console.log(resultImg);
+  } catch (error) {
+    res.status(404).send(error.message);
+  }
+});
+app.post("/isbn", async (req, res) => {
+  try {
+    var isbn = req.body["isbnSearch"];
+    const resultIsbn = await axios.get(`https://openlibrary.org/api/books?bibkeys=ISBN:${isbn}&format=json`);
+    var resultIsbnImg = resultIsbn.data;
+    var finalResult = resultIsbnImg[`ISBN:${isbn}`].thumbnail_url;
+    res.render("index.ejs", { 
+      anime: "",
+      character: "",
+      quote: "",
+      urlImage : "https://nekos.best/api/v2/hug/5721b0f4-a2ed-4de6-9ea3-763601c72fc9.gif", 
+      isbnData: finalResult,
+    });
+    console.log(resultIsbnImg);
+    console.log(finalResult);
   } catch (error) {
     res.status(404).send(error.message);
   }
 });
 
+var _OLBookInfo = 
+{
+  'ISBN:9781780992266': {
+    bib_key: 'ISBN:9781780992266',
+    info_url: 'https://openlibrary.org/books/OL31595593M/Ghosts_of_My_Life',
+    preview: 'noview',
+    preview_url: 'https://openlibrary.org/books/OL31595593M/Ghosts_of_My_Life',
+    thumbnail_url: 'https://covers.openlibrary.org/b/id/10526326-S.jpg'
+  }
+}
 
 
 app.listen(port, () => {
