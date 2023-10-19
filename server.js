ecd58ff3f227589432bc09cc2496dfd388776b56
petruda <petru650@gmail.com>
2023-10-19 16:24:25 +0300

    commented the server code

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
+// to use express for app dev
 import express from "express";
+// to use axios for req data from api's
 import axios from "axios";
 import bodyParser from "body-parser";
 import { dirname } from "path";
 // to take the url of the root
+//to tap into the body elements of the DOM
 import { fileURLToPath } from "url";
-// to make the full fath ( root + localpath)
-
 //dosen't do anything, but it was supposed to
 const __dirname = dirname(fileURLToPath(import.meta.url));
 //what port the server uses
@@ -17,10 +18,12 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 //for defining ejs as viewing engine
 app.set("view engine", "ejs");
-// strats here empty arrays for items for the lists
 
+// to handle te first req to load the main page
 app.get("/", (req, res) => {
     res.render("index.ejs", {    
+    
+    // left empty or with standard image so they are defined
       anime: "",
       character: "",
       quote: "",
@@ -28,13 +31,16 @@ app.get("/", (req, res) => {
       isbnData : "https://covers.openlibrary.org/b/id/8538817-S.jpg",
     });
   });
+//to hande the post request for generating an anime gif + quote
 app.post("/", async (req, res) => {
+  // Using axios to get from one API a quoate and from the other one an gif
   try {
     const result = await axios.get("https://animechan.xyz/api/random");
     const resultImage = await axios.get("https://nekos.best/api/v2/hug?amount=1");
+    //api response is an array of objects and we need to tap in the first objects url
     var resultImg = resultImage.data.results[0].url;
     
-
+// render the anime,charachter,quote and gif in the webpage
     res.render("index.ejs", { 
       anime: result.data.anime,
       character: result.data.character,
@@ -42,18 +48,26 @@ app.post("/", async (req, res) => {
       urlImage : resultImg,
       isbnData : "https://covers.openlibrary.org/b/id/8538817-S.jpg",
     });
+    //for testing the respong
     console.log(result);
     console.log(resultImg);
-  } catch (error) {
+  } catch (error) { // to handle errors
     res.status(404).send(error.message);
   }
 });
+// to handle isbn search req
 app.post("/isbn", async (req, res) => {
   try {
+    //storing the isbn in a var for easier reuse
     var isbn = req.body["isbnSearch"];
+    //getting the response from the Open Library api in a JSON format
     const resultIsbn = await axios.get(`https://openlibrary.org/api/books?bibkeys=ISBN:${isbn}&format=json`);
+    // to store initial response because otherwise throws error "ISBN:" is not defined
+    //since response is an object in an object
     var resultIsbnImg = resultIsbn.data;
+    //to tap into the isbn object and take just the URL for the cover of the book (very bad quality img)
     var finalResult = resultIsbnImg[`ISBN:${isbn}`].thumbnail_url;
+    //to render just the isbn response rest stays empty/standard
     res.render("index.ejs", { 
       anime: "",
       character: "",
@@ -61,25 +75,14 @@ app.post("/isbn", async (req, res) => {
       urlImage : "https://nekos.best/api/v2/hug/5721b0f4-a2ed-4de6-9ea3-763601c72fc9.gif", 
       isbnData: finalResult,
     });
+    // testing respnse
     console.log(resultIsbnImg);
     console.log(finalResult);
-  } catch (error) {
+  } catch (error) { // error handling
     res.status(404).send(error.message);
   }
 });
-
-var _OLBookInfo = 
-{
-  'ISBN:9781780992266': {
-    bib_key: 'ISBN:9781780992266',
-    info_url: 'https://openlibrary.org/books/OL31595593M/Ghosts_of_My_Life',
-    preview: 'noview',
-    preview_url: 'https://openlibrary.org/books/OL31595593M/Ghosts_of_My_Life',
-    thumbnail_url: 'https://covers.openlibrary.org/b/id/10526326-S.jpg'
-  }
-}
-
-
+// to log the port we are using
 app.listen(port, () => {
     console.log(`Server running on port: ${port}`);
   });
